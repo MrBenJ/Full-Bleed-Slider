37345ae447f0c396eab4df92d8e35d164713c7f4
Ben Junya <bjunya@gmail.com>
2015-01-26 20:52:15 -0800

    Incremental progress, figuring out how to add content w/o css psudo elements

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,9 +2,11 @@ $(document).ready(function() {
 
 	var images = document.getElementsByClassName('image');
 	var i = 0;
+	var slideshow = document.getElementById('slides');
+	
+	console.log(slideshow);
 
-	console.log(images[i]);
-
+	images[i].setAttribute('class', 'active'); // Sets the first image to be visible
 
 
 });
