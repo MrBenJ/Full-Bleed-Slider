859ddce11049af05172f469c93bdb2e20e3125cc
Ben Junya <ben.junya@72andsunny.com>
2015-01-28 13:46:51 -0800

    Added javascript carousel logic and code to be modular. Slider is now functional.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,11 +2,57 @@ $(document).ready(function() {
 
 	var images = document.getElementsByClassName('image');
 	var i = 0;
-	var slideshow = document.getElementById('slides');
-	
-	console.log(slideshow);
+	var currentImage = i;
+	var IMAGE_ACTIVE = 'image active';
+	var IMAGE_INACTIVE = 'image';
 
-	images[i].setAttribute('class', 'active'); // Sets the first image to be visible
+	var slideshow = document.getElementById('slides');
+
+	images[i].setAttribute('class', IMAGE_ACTIVE); // Sets the first image to be visible
+	
+	$('#back-button').click(function() {
+		console.log("Back Clicked");
+		images[currentImage].setAttribute('class', IMAGE_INACTIVE);
+
+
+		stepBackwards();
+
+	});
+
+	$('#forward-button').click(function() {
+		console.log("Forward Clicked");
+		images[currentImage].setAttribute('class', IMAGE_INACTIVE);
+
+		stepForwards();
+	})
+
+	function stepBackwards() {
+		i--;
+		console.log(i);
+		if (i < 0) {
+			i = (images.length - 1);
+			console.log("CONDITION HIT i = " + i);
+		}
+
+
+		images[i].setAttribute('class', IMAGE_ACTIVE);
+		currentImage = i;
+
+
+	}
+
+	function stepForwards() {
+		i++;
+				console.log(i);
+
+		if(i === images.length) {
+			i = 0;
+			console.log("CONDITION HIT i = " + i);
+		}
+
+		images[i].setAttribute('class', IMAGE_ACTIVE);
+		currentImage = i;
+	}
 
 
 });
